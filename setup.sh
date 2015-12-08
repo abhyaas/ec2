1aec9bd32ea41330902a79e8171c9bf8f78db2d2
Ubuntu <ubuntu@ip-172-31-22-45.us-west-2.compute.internal>
2015-12-08 07:42:22 +0000

    added some stuff

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+
+sudo apt-get update
+sudo apt-get upgrade
+
 ./setup_postgresql.sh
 
 
--- setup_postgresql.sh
+++ setup_postgresql.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
 
+
+sudo apt-get install postgresql
+
+
+sudo /etc/init.d/postgresql restart
+
+
+#https://github.com/snowplow/snowplow/wiki/Setting-up-PostgreSQL
+
+#sudo su - postgres
+#psql
+
+# CREATE USER power_user SUPERUSER;
+# ALTER USER power_user WITH PASSWORD 'password';
+# CREATE USER app_user NOSUPERUSER;
+# ALTER USER app_user WITH PASSWORD ‘password’;
+# CREATE DATABASE application WITH OWNER app_user;
+
+
+#sudo vim /etc/postgresql/9.3/main/pg_hba.conf
+#replace peer with md5
+
+#psql -U app_user application
+
