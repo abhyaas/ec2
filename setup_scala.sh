c4199943f275b97366d5c0341b42d4162f7e431a
Ubuntu <ubuntu@ip-172-31-21-90.us-west-2.compute.internal>
2015-12-13 08:52:17 +0000

    first checking

---
+++ setup_scala.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install default-jdk
+
+curl https://raw.githubusercontent.com/n8han/conscript/master/setup.sh | sh
+
+# edit .proifle and add : 
+#export PATH=$PATH:/home/ubuntu/bin/
+ 
+cs n8han/giter8
+
